3f7d9761100702cc0cb7cd95a4ff572659200bc3
Lucas L. S. Haine <lucaslshaine@gmail.com>
2023-11-03 19:41:27 -0300

    add script to install brave

---
+++ install_brave.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+apt-get install -q -y curl
+
+curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"| tee /etc/apt/sources.list.d/brave-browser-release.list
+
+apt-get update -y
+
+apt-get install -y brave-browser
